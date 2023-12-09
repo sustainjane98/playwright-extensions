ffdaa901447cc7f1c532e645ad253c90cbe273df
Jannik Will <jw19980309@gmail.com>
2023-12-09 19:35:18 +0100

    Update

--- apps/test-app-e2e/src/wait-for-css-change.spec.ts
+++ apps/test-app-e2e/src/wait-for-css-change.spec.ts
@@ -5,18 +5,29 @@ import { common } from '@test/pages';
 const test = common;
 
 test.describe('Wait For CSS Property Change', () => {
-  let element: Locator;
-
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
-
-    element = page.getByTestId(TestIds.WAIT_FOR_CSS_CHANGE);
   });
 
-  test('Wait for element change visibility', async ({ start }) => {
+  test('Wait for element change visibility', async ({ start, page }) => {
+    const element = page.getByTestId(TestIds.WAIT_FOR_CSS_CHANGE);
+
     await start.waitForAttributeChangeTimeout(element, 6000, {
       key: 'visibility',
       value: 'hidden',
+    });
+
+    await start.expectToExisting();
+    start.checkTheExecutionTime();
+    await expect(element).toHaveCount(1);
+  });
+
+  test('Wait for element change backgroundColor', async ({ start, page }) => {
+    const element = page.getByTestId(TestIds.WAIT_FOR_CSS_CHANGE_COLOR);
+
+    await start.waitForAttributeChangeTimeout(element, 6000, {
+      key: 'backgroundColor',
+      value: '#fff',
     });
 
     await start.expectToExisting();
